77706a4fd8d000412d18f1811a9bc96d92a96f2d
harshtomar67 <harsh.tomar@bonamisoftware.com>
2023-11-08 17:54:46 +0530

    feat: Add reply to review
    User(only admin) can reply to reviews from user on course

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -367,6 +367,54 @@ export const addReview = CatchAsyncErrors(
   }
 );
 
+
+interface IAddReplyData {
+   courseId: string,
+   reviewId: string,
+   comment: string
+}
+
+// add reply to review
+export const addReplyToReview = CatchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+      const {comment, courseId, reviewId} = req.body;
+
+      const course = await courseModel.findById(courseId);
+      if(!course) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+
+      const review = course.reviews.find((review: any) => review._id.equals(reviewId));
+      if(!review) {
+        return next(new ErrorHandler("Review not found", 404));
+      }
+
+      const newReply: any= {
+        user: req.user,
+        comment
+      };
+
+      if(!review.commentReplies) {
+        // console.log("yhaaaaaaaaa")
+        review.commentReplies = [];
+      }
+      review.commentReplies?.push(newReply);
+
+      // console.log(review, course);
+
+      await course?.save();
+
+      res.status(200).json({
+        success: true,
+        course
+      })
+    }
+    catch(error: any) {
+        return next(new ErrorHandler(error.message, 500));
+    }
+})
+
 // export const name = CatchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
 //     try {
 
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAnswer, addQuestion, addReview, editCourse, getAllCourses, getSingleCourse, getSingleCourseByUser, uploadCourse } from "../controllers/course.controller";
+import { addAnswer, addQuestion, addReplyToReview, addReview, editCourse, getAllCourses, getSingleCourse, getSingleCourseByUser, uploadCourse } from "../controllers/course.controller";
 import { isRole, isAuthenticated } from "../middleware/auth";
 
 const courseRouter = express.Router();
@@ -19,5 +19,7 @@ courseRouter.put('/question', isAuthenticated, addQuestion);
 courseRouter.put('/answer', isAuthenticated, addAnswer);
 
 courseRouter.put('/review/:id', isAuthenticated, addReview);
+
+courseRouter.put('/reply', isAuthenticated, isRole("admin"), addReplyToReview);
 
 export default courseRouter;
